bd9bb4f8df292ecb36cabcc6ddd1df40abf04d80
Maria Recskova <maria.recska@gmail.com>
2020-03-31 10:38:53 +0200

    device sizes and bignavbar

--- src/Components/BigNavbar.jsx
+++ src/Components/BigNavbar.jsx
@@ -1,14 +1,74 @@
 import React from "react";
-import "./BigNavbar.css";
+// import "./BigNavbar.css";
 import Logo from "./Images/Logo.svg";
+import GreyCorner from "./Images/GreyCorner.svg";
+import styled from "styled-components";
+import { Device } from "../Utils/Device";
+
+const BigNavContainer = styled.div`
+  display: flex;
+  position: relative;
+  flex-grow: 1;
+  flex-flow: row nowrap;
+  align-items: center;
+  margin: 0;
+  width: 100%;
+  max-height: 30%;
+  background-color: #ffffff;
+`;
+const LogoContainer = styled.div`
+  display: flex;
+  position: relative;
+  flex-grow: 1;
+  flex-direction: column;
+  min-width: 200px;
+  margin: 0;
+
+  @media ${Device.tablet} {
+    min-width: 300px;
+    flex-direction: row;
+  }
+  @media ${Device.laptop} {
+    min-width: 400px;
+  }
+`;
+const LogoBig = styled.img`
+  min-width: 70px;
+  margin: 0em;
+  justify-self: flex-start;
+  padding: 0.5em;
+
+  @media ${Device.laptop} {
+    min-height: 80px;
+  }
+`;
+
+const RightCorner = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  float: right;
+  margin: 0em;
+  padding: 0em;
+  margin-left: 50px;
+  min-height: 150px;
+  width: 150px;
+  background-image: url(${require(`./Images/GreyCorner.svg`)});
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  @media ${Device.laptop} {
+    margin-left: 250px;
+  }
+`;
 
 const BigNavbar = () => (
-  <div className="header-container">
-    <div className="logo-container">
-      <img className="image" src={Logo} alt={Logo} />
-    </div>
-    <div className="right-corner"></div>
-  </div>
+  <BigNavContainer>
+    <LogoContainer>
+      <LogoBig src={Logo} />
+    </LogoContainer>
+    <RightCorner />
+  </BigNavContainer>
 );
 
 export default BigNavbar;
