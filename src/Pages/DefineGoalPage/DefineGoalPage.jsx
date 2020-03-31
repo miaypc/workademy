e45746f9cce7b6a406f9c3a0c09304b2c25751b5
Elena <anel92r@gmail.com>
2020-03-31 17:38:30 +0200

    add goal input and depending on it button is clickable or not

--- src/Pages/DefineGoalPage/DefineGoalPage.jsx
+++ src/Pages/DefineGoalPage/DefineGoalPage.jsx
@@ -1,22 +1,34 @@
 import React from "react";
 import "./DefineGoalPage.scss";
+import GoalInput from "../../Components/Input/GoalInput";
 
 // importing styled components for page setup
 import {
   PageContainer,
   LeftBar,
   RightSection,
-  ButtonsContainer
+  ButtonsContainer,
+  Header
 } from "../StylePages";
+import { useState } from "react";
 
 function DefineGoalPage(props) {
+  //check if input is empty or not. If the goal isn't provided, we won't give user to access the next page
+  const [isGoalEmpty, setIsGoalEmpty] = useState(true);
+
+  function checkIsGoalEmpty(event) {
+    event.target.value ? setIsGoalEmpty(false) : setIsGoalEmpty(true);
+  }
+
   return (
     <PageContainer>
       <LeftBar>Define Goals</LeftBar>
       <RightSection>
+        <Header>Goal</Header>
+        <GoalInput checkIsGoalEmpty={checkIsGoalEmpty} />
         <ButtonsContainer>
           <button onClick={props.previousStep}>Previous Step</button>
-          <button onClick={props.nextStep}>Next Step</button>
+          <button onClick={!isGoalEmpty && props.nextStep}>Next Step</button>
         </ButtonsContainer>
       </RightSection>
     </PageContainer>
