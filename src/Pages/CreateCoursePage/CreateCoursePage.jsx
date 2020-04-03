2352afbef4aca81ccd38e75cc458965640149db0
Maria Recskova <maria.recska@gmail.com>
2020-04-03 12:33:44 +0200

    some styling changes

--- src/Pages/CreateCoursePage/CreateCoursePage.jsx
+++ src/Pages/CreateCoursePage/CreateCoursePage.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 import NewClassButton from "../../Components/NewClassButton";
 import BigNavbar from "../../Components/Navbars/BigNavbar";
-import CreateCourseWrapper from "./styleCreateCoursePage";
+import {
+  CreateCourseWrapper,
+  CreateCourseHeader,
+  CreateCourseH1,
+  CreateCourseH2,
+  NewClassButtonContainer
+} from "./styleCreateCoursePage";
 
 function CreateCoursePage(props) {
   return (
     <CreateCourseWrapper>
       <BigNavbar />
-      {/* <Header /> */}
-      <div className="bottomButtons">
-        <NewClassButton onClick={props.nextStep} />
-        <button onClick={props.nextStep}>Create your course</button>
-      </div>
+      <CreateCourseHeader>
+        <CreateCourseH1>
+          Welcome to the Workademy Course Builder !
+        </CreateCourseH1>
+        <CreateCourseH2>
+          To start creating your class, click on the button below
+        </CreateCourseH2>
+        <NewClassButtonContainer>
+          <NewClassButton onClick={props.nextStep} />
+          <button onClick={props.nextStep}>Create your course</button>
+        </NewClassButtonContainer>
+      </CreateCourseHeader>
     </CreateCourseWrapper>
   );
 }
--- src/Pages/CreateCoursePage/styleCreateCoursePage.jsx
+++ src/Pages/CreateCoursePage/styleCreateCoursePage.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const CreateCourseWrapper = styled.div`
+export const CreateCourseWrapper = styled.div`
   display: flex;
   flex-direction: column;
   max-width: 100%;
@@ -8,4 +8,36 @@ const CreateCourseWrapper = styled.div`
   margin: 0;
 `;
 
-export default CreateCourseWrapper;
+export const CreateCourseHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 100%;
+  max-height: 70%;
+`;
+
+export const CreateCourseH1 = styled.h1`
+  font-family: sans-serif;
+  text-align: center;
+  text-decoration: none;
+  font-size: 2.5em;
+  font-weight: normal;
+  color: #253858;
+  margin-top: 2em;
+`;
+
+export const CreateCourseH2 = styled.h2`
+  font-family: sans-serif;
+  text-align: center;
+  text-decoration: none;
+  font-size: 1.5em;
+  font-weight: normal;
+  color: #253858;
+`;
+
+export const NewClassButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 30%;
+  min-height: 10%;
+`;
