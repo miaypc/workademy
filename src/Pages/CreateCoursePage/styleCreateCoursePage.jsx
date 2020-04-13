2301337ed66c1dfe1c2d570825fab4546363d476
Maria Recskova <maria.recska@gmail.com>
2020-04-13 19:32:07 +0200

    small changes

--- src/Pages/CreateCoursePage/styleCreateCoursePage.jsx
+++ src/Pages/CreateCoursePage/styleCreateCoursePage.jsx
@@ -1,14 +1,10 @@
 import styled from "styled-components";
-import Color from "../../Utils/Color";
-import { LeftCorner } from "./styleCreateCoursePage";
 
 export const CreateCourseWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   flex-grow: 1;
-  max-width: 100%;
-  max-height: 100%;
   margin: 0;
   background-image: url(${require(`../../Components/Images/DownGreyCorner.svg`)});
   background-position: left bottom;
@@ -24,16 +20,3 @@ export const CreateCourseHeader = styled.div`
   text-align: center;
   margin: 0;
 `;
-
-/* export const LeftCorner = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex-grow: 1;
-  position: relative;
-  float: left;
-  padding: 0em;
-  min-height: 300px;
-  background-image: url(${require(`../../Components/Images/DownGreyCorner.svg`)});
-  background-position: right bottom;
-  background-repeat: no-repeat;
-`; */
