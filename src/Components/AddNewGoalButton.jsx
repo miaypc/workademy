1165cfffbac153071be343c5f57f67ceed2a4084
Maria Recskova <maria.recska@gmail.com>
2020-04-14 13:42:37 +0200

    small layout change

--- src/Components/AddNewGoalButton.jsx
+++ src/Components/AddNewGoalButton.jsx
@@ -6,29 +6,28 @@ import { Device } from "../Utils/Device";
 const Button = styled.button`
   display: flex;
   flex-shrink: 1;
-  align-self: center;
+  justify-self: center;
   font-family: sans-serif;
   text-align: left;
   text-decoration: none;
   border: none;
   border-radius: 5px;
-  padding: 0.5em;
-  margin: 2em;
+  padding: 0.7em;
+  margin: 3em 2em;
   background-color: ${Color.mainNavy};
   color: ${Color.mainWhite};
   cursor: pointer;
-  font-size: 1em;
+  font-size: 1.5em;
 
-  @media ${Device.mobileL} {
+  @media ${Device.tablet} {
     padding: 1em;
-    font-size: 1.5em;
-    margin: 3em;
+    font-size: 2em;
+    margin: 4em;
   }
 
   @media ${Device.laptop} {
     padding: 1em 4em 1em 4em;
     border-radius: 10px;
-    font-size: 2em;
     margin: 5em;
     background-image: url(${require(`../Components/Images/Plus.svg`)});
     background-position: right 20px top 20px;
