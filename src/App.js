3d4c038b2dccedbc5a364b3159b16e613dbf94c8
Maria Recskova <maria.recska@gmail.com>
2020-04-15 10:27:43 +0200

    commit changes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ import "./App.css";
 import NavigationWizard from "./Pages/Navigation/NavigationWizard";
 import { lightTheme } from "./Utils/LightTheme"
 import Button from '@material-ui/core/Button';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import CreateCoursePage from "./Pages/CreateCoursePage/CreateCoursePage";
 
 
 const App = () => (
 
   <div>
-    {/* <ThemeProvider theme={lightTheme}> */}
-    <NavigationWizard />
-    {/* </ThemeProvider> */}
+    <Router>
+      {/* <ThemeProvider theme={lightTheme}> */}
+      <Switch>
+        <Route exact path="/">
+          <CreateCoursePage />
+        </Route>
+        <Route path="/wizard">
+          <NavigationWizard />
+        </Route>
+      </Switch>
+      {/* </ThemeProvider> */}
+    </Router>
   </div>
 );
 
