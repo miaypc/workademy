1115304cbf8f7e55e80fec190694e1a0e5a136d3
Maria Recskova <maria.recska@gmail.com>
2020-04-17 12:45:58 +0200

    theme change

--- src/Utils/Themes/Theme.jsx
+++ src/Utils/Themes/Theme.jsx
@@ -4,12 +4,12 @@ export const lightTheme = {
   body: `${Color.mainWhite}`,
   text: `${Color.mainNavy}`,
   toggleBorder: `${Color.mainBlue}`,
-  gradient: "linear-gradient(`${Color.mainWhiteGrey}`, `${Color.mainBlue}`)",
+  gradient: `"linear-gradient(${Color.mainWhiteGrey}, ${Color.mainBlue})"`,
 };
 
 export const darkTheme = {
   body: `${Color.mainNavy}`,
   text: `${Color.mainWhite}`,
   toggleBorder: `${Color.mainWhiteGrey}`,
-  gradient: "linear-gradient(`${Color.mainBlue}`${Color.mainWhiteGrey}`)",
+  gradient: `"linear-gradient(${Color.mainBlue}, ${Color.mainWhiteGrey})"`,
 };
