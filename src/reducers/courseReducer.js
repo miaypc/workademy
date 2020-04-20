3bc20ffff16ab6ce2465a4ccb7abd6bcebe47b64
Elena <anel92r@gmail.com>
2020-04-20 15:51:41 +0200

    add goals to redux store

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,3 +1,5 @@
+// {courseName:"", courseDescription:"", goals:[{verb:"", name:""},{},{}]}
+
 export default function (state = {}, action) {
   switch (action.type) {
     case "CREATE_DESCRIPTION":
@@ -6,7 +8,17 @@ export default function (state = {}, action) {
         courseName: action.courseName,
         courseDescription: action.courseDescription,
       };
+    case "CREATE_GOAL":
+      return {
+        ...state,
+        goals: [
+          ...(state.goals || []),
+          { verb: action.verb, name: action.name },
+        ],
+      };
     default:
       return state;
   }
 }
+
+// (a, b) => a > b? a:b
