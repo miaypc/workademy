b1af5982026407a56020f693537dacf20ea036e6
Maria Recskova <maria.recska@gmail.com>
2020-04-20 17:36:21 +0200

    fixed font size and padding in mobile view

--- src/Components/AddNewGoalButton.jsx
+++ src/Components/AddNewGoalButton.jsx
@@ -5,30 +5,29 @@ import { Device } from "../Utils/Device";
 
 const Button = styled.button`
   display: flex;
-  flex-shrink: 1;
   justify-self: center;
   font-family: sans-serif;
   text-align: left;
   text-decoration: none;
-  border: none;
+  border: 0em;
   border-radius: 5px;
-  padding: 0.7em;
-  margin: 3em 2em;
+  padding: 1em;
+  /* margin: 0.5; */
+  margin: 5em 0em 5em;
   background-color: ${Color.mainNavy};
   color: ${Color.mainWhite};
   cursor: pointer;
-  font-size: 1.5em;
+  font-size: 1em;
 
   @media ${Device.tablet} {
-    padding: 1em;
+    padding: 1.5em;
     font-size: 2em;
     margin: 4em;
+    border-radius: 10px;
   }
 
   @media ${Device.laptop} {
     padding: 1em 4em 1em 4em;
-    border-radius: 10px;
-    margin: 5em;
     background-image: url(${require(`../Components/Images/Plus.svg`)});
     background-position: right 20px top 20px;
     background-size: 10%;
