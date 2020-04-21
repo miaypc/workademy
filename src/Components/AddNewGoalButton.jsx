21ed4dd29c621593f8d05fc81ec92dc91e1f6d4a
Mia <55692126+miaypc@users.noreply.github.com>
2020-04-21 09:44:59 +0200

    Revert "Light dark switch responsitity"

--- src/Components/AddNewGoalButton.jsx
+++ src/Components/AddNewGoalButton.jsx
@@ -5,28 +5,30 @@ import { Device } from "../Utils/Device";
 
 const Button = styled.button`
   display: flex;
+  flex-shrink: 1;
   justify-self: center;
   font-family: sans-serif;
   text-align: left;
   text-decoration: none;
-  border: 0em;
+  border: none;
   border-radius: 5px;
-  padding: 1em;
-  margin: 5em 0em 5em;
+  padding: 0.7em;
+  margin: 3em 2em;
   background-color: ${Color.mainNavy};
   color: ${Color.mainWhite};
   cursor: pointer;
-  font-size: 1em;
+  font-size: 1.5em;
 
   @media ${Device.tablet} {
-    padding: 1.5em;
+    padding: 1em;
     font-size: 2em;
     margin: 4em;
-    border-radius: 10px;
   }
 
   @media ${Device.laptop} {
     padding: 1em 4em 1em 4em;
+    border-radius: 10px;
+    margin: 5em;
     background-image: url(${require(`../Components/Images/Plus.svg`)});
     background-position: right 20px top 20px;
     background-size: 10%;
