41226e82e09132a668a118ee98df5a3604b19626
Elena <anel92r@gmail.com>
2020-04-27 18:24:35 +0200

    move evaluation page styles to a separate page

--- src/Pages/EvaluationPage/EvaluationPage.jsx
+++ src/Pages/EvaluationPage/EvaluationPage.jsx
@@ -1,49 +1,18 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 import { connect } from "react-redux";
-import "./EvaluationPage.scss";
+import { MainContent, SmallParagraph, Card, Text } from "./EvaluationPage.js";
 import { NavigationButton } from "../../Components/styleButton";
 import { RightSection, ButtonsContainer, BlueTobBar } from "../StylePages";
 import QuestionList from "../../Components/EvaluationContainer/QuestionList";
-import _Card from "@material-ui/core/Card";
-import Color from "../../Utils/Color";
 import QuestionTypes from "../../Components/EvaluationContainer/QuestionTypes";
 import Answers from "../../Components/EvaluationContainer/Answers";
-
-const MainContent = styled.div`
-  display: flex;
-  margin-top: 5%;
-`;
-
-const SmallParagraph = styled.p`
-  color: red;
-  font-size: 8px;
-  display: flex;
-  justify-content: flex-end;
-  padding-right: 10px;
-`;
-
-const Card = styled(_Card)`
-  width: 75%;
-
-  margin-left: 1%;
-  border: 5px solid ${Color.mainNavy};
-  display: flex;
-  flex-direction: column;
-`;
-const Text = styled.div`
-  color: ${Color.mainNavy};
-  font-size: 1.5em;
-  text-align: center;
-  margin: 10px;
-`;
 
 function EvaluationPage(props) {
   const [isErrorVisible, setIsErrorVisible] = useState(false); // Check if user filled all the fields if no - show error
   const [questionName, setQuestionName] = useState();
   const [qType, setQType] = useState(); //Three question types are possible: single-choice, multiple-choice, free-text
   const [answers, setAnswers] = useState(["", "", "", ""]); // Array with 4 answers or nothing for free text.
-  const [correctAnswer, setCorrectAnswer] = useState([]); // Array of correct answers starting from 1
+  const [correctAnswer, setCorrectAnswer] = useState([]); // Array of correct answers starting from 1 or nothing for free text
 
   function showErrorMessage() {
     if (isErrorVisible) {
