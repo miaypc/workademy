b6e63c0dca223270bef76817176395aa4d439e34
Elena <anel92r@gmail.com>
2020-04-28 11:35:56 +0200

    work in progress

--- src/Components/EvaluationContainer/QuestionList.jsx
+++ src/Components/EvaluationContainer/QuestionList.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-// import AddNewContentButton from "../AddNewContentButton";
+import AddNewContentButton from "../AddNewContentButton";
+// import Bin from "../Images/Bin.svg";
 
 const Container = styled.div`
   display: flex;
@@ -8,7 +9,41 @@ const Container = styled.div`
 `;
 
 function QuestionList({ questions }) {
-  return <Container></Container>;
+  // for others
+
+  // for add content on the side
+  const handleDeleteQuestion = () => {
+    // setContents(contents.filter((element) => element !== link));
+  };
+
+  const handlePlusSubmit = () => {
+    // setHint("Please add a content");
+    setInterval(() => {
+      // setHint("");
+    }, 3000);
+  };
+  return (
+    <Container>
+      {/* <ol>
+        {questions.map((question) => (
+          <li>Question</li>
+        ))}
+      </ol> */}
+
+      <ol>
+        <AddNewContentButton
+          contents={questions}
+          handleDeleteContent={handleDeleteQuestion}
+          handlePlusSubmit={handlePlusSubmit}
+          ContentText="Question"
+          handleDeleteTextContent={() => {
+            return;
+          }}
+          textContents={[]}
+        />
+      </ol>
+    </Container>
+  );
 }
 
 export default QuestionList;
