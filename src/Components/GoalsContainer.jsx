957a112b85caad79389279accb0c9673d0f7f424
Mia Yin <me@miayin.com>
2020-04-29 15:50:09 +0200

    added dropdown menu to goal container

--- src/Components/GoalsContainer.jsx
+++ src/Components/GoalsContainer.jsx
@@ -3,10 +3,10 @@ import Color from "../Utils/Color";
 import styled from "styled-components";
 import _Card from "@material-ui/core/Card";
 import _Typography from "@material-ui/core/Typography";
-//import ThreeDots from "./Images/ThreeDots.svg";
 import _MoreIcon from "@material-ui/icons/MoreVert";
 import IconButton from "@material-ui/core/IconButton";
 import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
 
 const Card = styled(_Card)`
   && {
@@ -31,26 +31,43 @@ const Typography = styled(_Typography)`
   font-size: 20px;
 `;
 
-const ThreeDotsContainer = styled.img`
-  width: 5px;
-  cursor: pointer;
-`;
-
 const MoreIcon = styled(_MoreIcon)`
   color: ${Color.mainWhite};
 `;
 
 function GoalsContainer({ goal }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Card>
       <Typography>{goal.verb + "" + goal.name}</Typography>
-      {/* <ThreeDotsContainer src={ThreeDots} /> */}
-      <IconButton edge="end" size="large" aria-label="display more actions">
-        <MoreIcon size="large">
-          <MenuItem>Edit</MenuItem>
-          <MenuItem>Delete</MenuItem>
-        </MoreIcon>
+      <IconButton
+        edge="end"
+        size="large"
+        aria-label="display more actions"
+        onClick={handleClick}
+      >
+        <MoreIcon />
       </IconButton>
+      <Menu
+        size="large"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem>Edit</MenuItem>
+        <MenuItem>Delete</MenuItem>
+        <MenuItem>Evaluation</MenuItem>
+      </Menu>
     </Card>
   );
 }
