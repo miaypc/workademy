ebc45ce2064fa8a12412fa85d70941bd8177f9a5
Maria Recskova <maria.recska@gmail.com>
2020-05-05 13:25:20 +0200

    first commit- work in progress

--- src/Components/Navbars/ProgressBar.jsx
+++ src/Components/Navbars/ProgressBar.jsx
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Stepper from "@material-ui/core/Stepper";
 import Step from "@material-ui/core/Step";
 import StepLabel from "@material-ui/core/StepLabel";
+import StepButton from "@material-ui/core/StepButton";
 import StepContent from "@material-ui/core/StepContent";
 import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
@@ -74,8 +75,54 @@ export default function ProgressBar({ currentStep }) {
   const classes = useStyles();
   // const [activeStep, setActiveStep] = React.useState(currentStep);
   const activeStep = currentStep - 1;
+  const [completed, setCompleted] = React.useState({});
   const steps = getSteps();
 
+  const totalSteps = () => {
+    return steps.length;
+  };
+
+  const completedSteps = () => {
+    return Object.keys(completed).length;
+  };
+
+  const isLastStep = () => {
+    return activeStep === totalSteps() - 1;
+  };
+
+  const allStepsCompleted = () => {
+    return completedSteps() === totalSteps();
+  };
+
+  const handleNext = () => {
+    const newActiveStep =
+      isLastStep() && !allStepsCompleted()
+        ? // It's the last step, but not all steps have been completed,
+          // find the first step that has been completed
+          steps.findIndex((step, i) => !(i in completed))
+        : activeStep + 1;
+    // setActiveStep(newActiveStep);
+  };
+
+  // const handleBack = () => {
+  //   setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  // };
+
+  const handleStep = (step) => () => {
+    // setActiveStep(step);
+  };
+
+  // const handleComplete = () => {
+  //   const newCompleted = completed;
+  //   newCompleted[activeStep] = true;
+  //   setCompleted(newCompleted);
+  //   handleNext();
+  // };
+
+  // const handleReset = () => {
+  //   setActiveStep(0);
+  //   setCompleted({});
+  // };
   // const handleNext = () => {
   //   setActiveStep((prevActiveStep) => prevActiveStep + 1);
   // };
@@ -84,9 +131,9 @@ export default function ProgressBar({ currentStep }) {
   //   setActiveStep((prevActiveStep) => prevActiveStep - 1);
   // };
 
-  const handleReset = () => {
-    // setActiveStep(0);
-  };
+  // const handleReset = () => {
+  // setActiveStep(0);
+  // };
 
   return (
     <ThemeProvider theme={theme}>
@@ -98,21 +145,20 @@ export default function ProgressBar({ currentStep }) {
         >
           {steps.map((label, index) => (
             <Step key={label}>
-              <StepLabel>{label}</StepLabel>
+              <StepButton
+                onClick={handleStep(index)}
+                completed={completed[index]}
+              >
+                {label}
+              </StepButton>
+              {/* <StepLabel>{label}
+              </StepLabel> */}
               <StepContent>
                 <div className={classes.actionsContainer}></div>
               </StepContent>
             </Step>
           ))}
         </Stepper>
-        {activeStep === steps.length && (
-          <Paper square elevation={0} className={classes.resetContainer}>
-            <Typography>All steps completed - you&apos;re finished</Typography>
-            <Button onClick={handleReset} className={classes.button}>
-              Reset
-            </Button>
-          </Paper>
-        )}
       </div>
     </ThemeProvider>
   );
